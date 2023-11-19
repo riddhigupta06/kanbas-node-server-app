178257818abbddfc9c363f527b77131f18461af4
Riddhi Gupta <riddhig566@gmail.com>
2023-11-19 18:57:18 -0500

    implemented kanabs courses

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from "cors";
 import Hello from "./hello.js"
 import Lab5 from './Lab5.js';
+import CourseRoutes from "./courses/routes.js";
 
 const app = express()
 app.use(cors());
@@ -9,5 +10,6 @@ app.use(express.json());
 
 Hello(app)
 Lab5(app);
+CourseRoutes(app);
 
 app.listen(4000)
