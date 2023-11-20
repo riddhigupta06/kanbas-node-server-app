c0bff28f5eb9476a9d380c7e5b0edecca536519f
Riddhi Gupta <riddhig566@gmail.com>
2023-11-19 20:13:23 -0500

    fixed api urls

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from "cors";
 import "dotenv/config";
-import session from "express-session";
 import Hello from "./hello.js"
 import Lab5 from './lab5.js';
 import CourseRoutes from "./courses/routes.js";
