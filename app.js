9342ef95d3f04f44d8165c55acdd604e989ffdd9
Riddhi Gupta <riddhig566@gmail.com>
2023-11-19 19:14:54 -0500

    finished Kanbas

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import Hello from "./hello.js"
 import Lab5 from './Lab5.js';
 import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
 
 const app = express()
 app.use(cors());
@@ -11,5 +12,6 @@ app.use(express.json());
 Hello(app)
 Lab5(app);
 CourseRoutes(app);
+ModuleRoutes(app);
 
 app.listen(4000)
